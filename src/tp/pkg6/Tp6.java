a4fff3dbbbe212f7011536c38f8d06836e7c2d53
Pablo <Pablo@NOTEBOOK-PABLO>
2020-09-28 00:50:17 -0300

    Comit inicial

---
+++ src/tp/pkg6/Tp6.java
@@ -0,0 +1,14 @@
+
+package tp.pkg6;
+
+
+public class Tp6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
